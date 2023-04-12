11af60063c3d3cc85b2d66e57212bdfe6e94a33a
Leticia Leal <leticialealp@hotmail.com>
2023-04-12 20:58:43 -0300

    adicionado configuração de eslint e prettier

--- src/configs/routes/AppRoutes.tsx
+++ src/configs/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Welcome from '../../pages/Welcome';
 
 const AppRoutes: React.FC = () => {
--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import reactLogo from '/assets/react.svg';
 import viteLogo from '/assets/vite.svg';
 
@@ -6,10 +7,10 @@ const Welcome: React.FC = () => {
 	return (
 		<div className="App">
 			<div>
-				<a href="https://vitejs.dev" target="_blank">
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
-				<a href="https://reactjs.org" target="_blank">
+				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
 					<img
 						src={reactLogo}
 						className="logo react"
